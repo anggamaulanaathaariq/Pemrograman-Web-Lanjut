58b776e0febf94eb8b444c91e23a6bc2fe5492c8
anggamaulanaathaariq <am.athaariq@gmail.com>
2019-02-20 20:44:16 +0700

    Update courses.service.ts

--- src/app/courses/courses.service.ts
+++ src/app/courses/courses.service.ts
@@ -9,16 +9,11 @@ export class CoursesService {
 
   getCourses(){
     return [
-    // {id: 0, name: 'HTML'},
-    // {id: 1, name: 'PHP'},
-    // {id: 2, name: 'ANGULAR'},
-    // {id: 3, name: 'C#'},
-    // {id: 4, name: 'VB.NET'},
-    {no: 1, Hobby: 'berlari', Keterangan: 'Lari 1 jam tiap hari'},
-    {no: 2, Hobby: 'senam', Keterangan: 'Senam tiap sore'},
-    {no: 3, Hobby: 'bermain musik', Keterangan: 'Bermain musik saat waktu senggang'},
-    // {name: 'author2'},
-    // {name: 'author3'},
-  ];
+    {id: 0, name: 'HTML'},
+    {id: 1, name: 'PHP'},
+    {id: 2, name: 'ANGULAR'},
+    {id: 3, name: 'C#'},
+    {id: 4, name: 'VB.NET'},
+    ]
   }
 }
