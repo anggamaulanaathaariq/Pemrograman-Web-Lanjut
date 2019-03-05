5df28edc6cf38eae61542082f1f95f8d40f2ec00
anggamaulanaathaariq <am.athaariq@gmail.com>
2019-03-05 22:33:36 +0700

    Update JS5

--- Jobsheet5/src/app/servers/servers.component.ts
+++ Jobsheet5/src/app/servers/servers.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   template: `<h1> hello {{name}} </h1>`,
-  styleUrls: ['./servers.component.css']
+  // styleUrls: ['./servers.component.css']
+styles:[`
+  h1 {
+    color:dodgerblue;
+  }`]
 })
 export class ServersComponent implements OnInit {
 
